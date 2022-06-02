47bf5de7bf18736d2b98f2abf182a219bb197e1b
Erdal ALTIN <erdalaltin@gmail.com>
2022-06-02 12:26:19 +0300

    Initial commit

---
+++ html/index.php
@@ -0,0 +1,20 @@
+<?php
+
+phpinfo();
+
+/*
+$link = mysqli_connect( 'database', 'DB_USER', 'DB_PASSWORD', 'DB_NAME' );
+
+//if connection is not successful you will see text error
+if ( !$link ) {
+       die( 'Could not connect: ' . mysql_error() );
+}
+
+//if connection is successfully you will see message below
+echo 'Connected successfully';
+echo "Host information: " . mysqli_get_host_info( $link ) . PHP_EOL;
+ 
+mysqli_close( $link );
+*/
+
+?>
