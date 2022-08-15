78dd45035c57e3874764b7605ee9aeaf0b612061
Erdal ALTIN <erdalaltin@gmail.com>
2022-08-15 10:29:05 +0300

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,5 +1,12 @@
 <?php
 
+//Connecting to Redis server on localhost 
+$redis = new Redis(); 
+$redis->connect('redis', 6379); 
+echo "Connection to server sucessfully"; 
+//check whether server is running or not 
+echo "Server is running: " . $redis->ping(); 
+
 phpinfo();
 
 /*
