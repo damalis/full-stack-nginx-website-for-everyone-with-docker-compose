79b353c7ae050a41d5ab359306c28241f83d86bd
Erdal ALTIN <erdalaltin@gmail.com>
2022-11-04 18:17:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -250,8 +250,8 @@ if [ -x "$(command -v docker)" ] && [ -x "$(command -v docker-compose)" ]; then
 				if [ ! -z `docker ps -q -f "status=running" --no-trunc | grep $(docker-compose ps -q webserver)` ]; then break; fi
 			done			
 			echo ""
-			echo "Reloading Webserver Nginx ssl configuration"
-			docker exec webserver nginx -s reload > /dev/null 2>&1
+			echo "Reloading webserver ssl configuration"
+			docker container restart webserver > /dev/null 2>&1
 			echo "Ok."
 			echo ""
 			echo "completed setup"
